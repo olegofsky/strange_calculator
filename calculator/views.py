d7ada4b36b3976970fefa70ecbc52c739ace41c9
Shiryaev Oleg <olegofsky@gmail.com>
2015-02-28 01:24:19 +0300

    corrected arithmetic_operations

--- calculator/views.py
+++ calculator/views.py
@@ -19,10 +19,7 @@ def arithmetic_operations(x, y, oper):
     str_x = "Decimal(\"%s\")" % x
     str_y = "Decimal(\"%s\")" % y
     result = eval(" ".join([str_x, oper, str_y]))
-    result = result.to_eng_string()
-    if result.endswith(".0"):
-        result = result.replace(".0", "")
-    return result
+    return result.to_eng_string().replace(".0", "")
 
 
 def process_operator(oper):
@@ -55,58 +52,59 @@ def process_operator(oper):
                 return "0"
 
 
-# def process_digit(digit):
-#     a = cache.get("a")
-#     b = cache.get("b")
-#     operator = cache.get("oper")
-#     if operator and b:
-#         if digit in ".":
-#             if digit in b:
-#                 return b
-#             else:
-#                 b += digit
-#                 cache.set("b", b)
-#                 return b
-#         else:
-#             b += digit
-#             cache.set("b", b)
-#             return b
-#     elif operator and not b:
-#         if digit in ".":
-#             b = "0" + digit
-#             cache.set("b", b)
-#             return b
-#         else:
-#             b = digit
-#             cache.set("b", b)
-#             return b
-#     elif a and not operator:
-#         if digit in "." and digit in a:
-#                 return a
-#         elif digit in "." and a in "-":
-#                 return a
-#         else:
-#             a += digit
-#             cache.set("a", a)
-#             return a
-#     elif not a:
-#         if digit in ".":
-#             a = "0" + digit
-#             cache.set("a", a)
-#             return a
-#         else:
-#             a = digit
-#             cache.set("a", a)
-#             return a
-
 def process_digit(digit):
     a = cache.get("a")
     b = cache.get("b")
     operator = cache.get("oper")
-    if operator:
-        pass
-    else:
-        pass
+    if operator and b:
+        if digit in ".":
+            if digit in b:
+                return b
+            else:
+                b += digit
+                cache.set("b", b)
+                return b
+        else:
+            b += digit
+            cache.set("b", b)
+            return b
+    elif operator and not b:
+        if digit in ".":
+            b = "0" + digit
+            cache.set("b", b)
+            return b
+        else:
+            b = digit
+            cache.set("b", b)
+            return b
+    elif a and not operator:
+        if digit in "." and digit in a:
+                return a
+        elif digit in "." and a in "-":
+                return a
+        else:
+            a += digit
+            cache.set("a", a)
+            return a
+    elif not a:
+        if digit in ".":
+            a = "0" + digit
+            cache.set("a", a)
+            return a
+        else:
+            a = digit
+            cache.set("a", a)
+            return a
+
+
+# def process_digit(digit):
+#     a = cache.get("a")
+#     b = cache.get("b")
+#     operator = cache.get("oper")
+#     if operator:
+#         pass
+#     else:
+#         pass
 
 
 def process_command(command):
@@ -122,7 +120,8 @@ def process_command(command):
         elif not b:
             return a
         elif b.replace("0", "") == ".":
-            return "Infinity"
+            cache.clear()
+            return "Error"
         else:
             a = arithmetic_operations(a, b, stored_operator)
             cache.set("a", a)
